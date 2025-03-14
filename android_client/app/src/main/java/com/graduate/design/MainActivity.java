12230184df45615e4ecac143f4088a65bb6d5c06
guoxingyu <guoxingyu240014@gmail.com>
2025-03-14 15:55:54 +0800

    change-bg

--- android_client/app/src/main/java/com/graduate/design/MainActivity.java
+++ android_client/app/src/main/java/com/graduate/design/MainActivity.java
@@ -20,6 +20,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        getWindow().getDecorView().setSystemUiVisibility(
+//                View.SYSTEM_UI_FLAG_FULLSCREEN
+//                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
         setContentView(R.layout.activity_main);
 
         // 初始化页面
